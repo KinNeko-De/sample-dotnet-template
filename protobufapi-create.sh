829635bf6de3fa243d0dcb8c773ec8f2dc6742a6
Nils Diekmann <github@kinneko.de>
2025-03-15 15:12:24 +0100

    - add parameter for targetframework

--- protobufapi-create.sh
+++ protobufapi-create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DOTNET_CLI_UI_LANGUAGE=en
-dotnet new protobufapi -o testoutput/protobufapi/allparameters --protoRoot testproto --company Kindatta --apiproject ECommerce --apiidentifier OrderProduct
+dotnet new protobufapi -o testoutput/protobufapi/allparameters --protoRoot testproto --company Kindatta --apiproject ECommerce --apiidentifier OrderProduct --framework net8.0 net9.0
