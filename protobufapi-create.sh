151dcf5b8bb77bb9408e79ea31adca7d82a54419
Nils Diekmann <github@kinneko.de>
2025-03-15 14:49:32 +0100

    - add parameter for api identifier

--- protobufapi-create.sh
+++ protobufapi-create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DOTNET_CLI_UI_LANGUAGE=en
-dotnet new protobufapi -o testoutput/protobufapi/allparameters --protoRoot testproto --company Kindatta --apiproject ECommerce
+dotnet new protobufapi -o testoutput/protobufapi/allparameters --protoRoot testproto --company Kindatta --apiproject ECommerce --apiidentifier OrderProduct
