2994c8d20fd4ef331350222c6877628785eced27
Nils Diekmann <github@kinneko.de>
2025-03-15 14:07:15 +0100

    - add parameter for company

--- protobufapi-create.sh
+++ protobufapi-create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DOTNET_CLI_UI_LANGUAGE=en
-dotnet new protobufapi -o testoutput/protobufapi/allparameters --protoRoot testproto
+dotnet new protobufapi -o testoutput/protobufapi/allparameters --protoRoot testproto --company Kindatta
